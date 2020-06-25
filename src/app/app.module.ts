4466e020a7e60029ffccf2c2ed7f1391bc164e1e
DRodriGerrard <drodrigerrard@gmail.com>
2020-06-25 18:45:38 +0100

    npm installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './components/master/master.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent,
+    DetailsComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
